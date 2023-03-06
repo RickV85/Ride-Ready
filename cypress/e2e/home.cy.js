77f2f26a75e34320d6bed74cae4d98fc1922c9f0
Rick Vermeil <rickv85@gmail.com>
2023-03-05 20:14:15 -0700

    Add test to home for 404

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -8,4 +8,9 @@ describe('Home', () => {
     cy.get('h2').should('have.text', 'Your personal suspension manager')
     cy.get('button').should('have.text', 'Log in with Strava')
   })
+
+  it('Should show an error on a bad url request', () => {
+    cy.visit('http://localhost:3000/ergheth')
+    cy.get('p').should('have.text', 'Oops, no page exists here. Sending you back to the home page.')
+  })
 })
